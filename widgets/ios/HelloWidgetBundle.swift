5da7ce9a6cae093885ed1bb14c2d664b76835ce8
Gokay Titrek <gokay@endometriose.app>
2024-05-22 15:39:49 +0200

    add widget/ios folder

---
+++ widgets/ios/HelloWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  HelloWidgetBundle.swift
+//  HelloWidget
+//
+//  Created by Gokay on 22.05.2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct HelloWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        HelloWidget()
+    }
+}
